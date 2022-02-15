8bab7881c8561df1388f958da2ab306caab34ae0
jaylon.cheng <jaylon.cheng@peliplat.com>
2022-02-15 12:57:33 +0800

    init springboot project

---
+++ src/main/java/com/hksz/demo/HkszdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hksz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkszdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HkszdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hksz/demo/HkszdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hksz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkszdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
