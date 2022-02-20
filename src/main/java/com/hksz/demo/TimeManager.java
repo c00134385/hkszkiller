4f9ad1a3436673ce45c3e57bd6268dddba1424eb
jaylon.cheng <jaylon.cheng@peliplat.com>
2022-02-20 22:43:44 +0800

    update timemanager

--- src/main/java/com/hksz/demo/TimeManager.java
+++ src/main/java/com/hksz/demo/TimeManager.java
@@ -8,9 +8,11 @@ public class TimeManager {
     private static Calendar calendar = Calendar.getInstance();
 
     public static Date beginTime() {
-        calendar.set(Calendar.HOUR_OF_DAY, 22);
-        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         calendar.setTimeInMillis(calendar.getTimeInMillis() + Configure.timeOffset);
         return calendar.getTime();
     }
@@ -19,11 +21,4 @@ public class TimeManager {
         return new Date(beginTime().getTime() + 10 * 1000);
     }
 
-    public static Date endTimeOfQueryRooms() {
-        calendar.set(Calendar.HOUR_OF_DAY, 9);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 30);
-        calendar.setTimeInMillis(calendar.getTimeInMillis());
-        return calendar.getTime();
-    }
 }
